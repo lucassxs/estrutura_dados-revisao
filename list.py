a71d3eb439cc873faeeee0bbb3508aeed9712577
Lucas Stefano <lucassxs.sp@gmail.com>
2022-07-24 18:45:22 -0300

    first commit

---
+++ list.py
@@ -0,0 +1,37 @@
+### Revisão 01
+### 22 Julho 2022
+
+a = 10
+b = 20
+
+b+a 
+
+nome = 'lucas'
+print('o meu nome é',nome)
+print(b+a)
+
+### 
+
+nome = input('Digite seu nome: ')
+idade = int(input('DIgite sua idade: '))
+
+print('Ola', nome)
+print(idade, 'anos')
+
+
+lucas=75.68
+round(lucas)
+
+### 
+p=75.25
+l=3.5
+
+u=round(p+l)
+u
+
+
+#### abs
+#####
+######
+
+abs(p+l)
